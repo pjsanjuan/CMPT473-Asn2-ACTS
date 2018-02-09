101cc1d9da41b78e48a27feff7a8a348945d1281
Unknown <Jordanhho12@hotmail.com>
2018-02-08 17:26:26 -0800

    Added requirements of output test message

--- runTests.py
+++ runTests.py
@@ -1,4 +1,6 @@
 import os
+import io
+import filecmp
 
 #from subprocess import call
 #Authors: Jordan Ho, Patrick San Juan
@@ -9,39 +11,70 @@ import os
 #global variables
 inputFilePath = 'TestData/TestFiles/TestInput/Files/'
 outputFilePath = 'TestData/TestFiles/TestOutput/Files/'
+outputMessagePath = 'TestData/TestFiles/TestOutput/Messages/'
 expectedOutputPath = 'TestData/ExpectedOutput/'
-testNum = 0
+testNum = 1
 
 print '\njson2csv program Testing'
 print '-----------------------------------------'
 
-def runJSONToCSV(csvFields, inputFile, outputFile):
-	cmd = 'json2csv -k ' + csvFields + ' -i ' + inputFilePath + inputFile + ' -o ' + outputFilePath + outputFile
-	print cmd
+def getfileName(fileName):
+	global testNum
+	return ('test' + str(testNum) + '_' + fileName);
+
+
+def runJSONToCSV(csvFields, inputFile):
+	outputFile = outputFilePath + getfileName('output.csv')
+	cmd = 'json2csv -k ' + csvFields + ' -i ' + inputFilePath + inputFile + ' -o ' + outputFile
+	print 'running cmd' + cmd + '\n'
 	os.system(cmd)
 	return; 
 
-def compareFilesOutput(outputFile, expectedOutputFile):
-	cmd = 'diff ' + expectedOutputPath + expectedOutputFile + ' ' + outputFilePath + outputFile
-	print cmd
-	os.system(cmd)
+
+def compareFilesOutput_pythonCMP():
+	outputFile = outputFilePath + getfileName('output.csv')
+	expectedOutputFile = expectedOutputPath + getfileName('ExpectedOutput.csv')
+	outputMessageFile = outputMessagePath + getfileName('message.txt')
+
+	cmd = 'diff -q ' + outputFile + ' ' + expectedOutputFile
+	print 'running cmd' + cmd + '\n'
+	os.system(cmd + ' > ' + outputMessageFile)
+
+	print 'test message is output to ' + outputMessageFile + '\n'
+
+	fileIsSame = filecmp.cmp(outputFile, expectedOutputFile)
+	if fileIsSame:
+		message = 'Both Files are identical, TEST PASSED'
+		print message
+		addMessageOutput(message)
+	else:
+		message = 'Files are different, TEST FAILED'
+		print message
+		addMessageOutput(message)
 	return; 
 
-def testFrame(testName, csvFields, inputFile, outputFile, expectedOutputFile):
+
+def addMessageOutput(message):
+	global testNum
+	with io.FileIO(outputMessagePath + getfileName('message.txt'), 'a') as file:
+			file.write(message)
+
+
+def testFrame(testName, csvFields, inputFile):
 	global testNum
 	print '\n 	[Test: ' + str(testNum) + ' - ' + testName + ']'
 	print '-----------------------------------------'
-	runJSONToCSV(csvFields, inputFile, outputFile)
-	compareFilesOutput(outputFile, expectedOutputFile)
+	runJSONToCSV(csvFields, inputFile)
+	compareFilesOutput_pythonCMP()
 	testNum += 1
 	return;
 
 
 #do testing here
-#testFrame('Test name', 'field1,field2', 'input.json', 'output.csv', 'outputCorrect.csv')
+#testFrame('Test name', 'field1,field2', 'input.json')
 
 #test 1
-testFrame('Testing something', 'user.name,remote_ip', 'test1_input.json', 'test1_output.csv', 'test1_ExpectedOutput.csv')
+testFrame('Testing something', 'user.name,remote_ip', 'test1_input.json')
 
 #test 2
 #TODO

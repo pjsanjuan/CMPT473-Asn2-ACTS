d018efcabcc86af9fb528bd5ebf514095bdc4440
Unknown <Jordanhho12@hotmail.com>
2018-02-09 20:43:08 -0800

    Completed tests 1 to 3
    WIP comparison of messages is not working for some reason

--- runTests.py
+++ runTests.py
@@ -23,17 +23,6 @@ def getfileName(fileName):
 	global testNum
 	return ('test' + str(testNum) + '_' + fileName);
 
-def compareExpectedMessageOutput(messageFile, expectedMessageFile):
-	messageIsSame = filecmp.cmp(messageFile, expectedMessageFile)
-	if messageIsSame:
-		message = 'Both messages are same'
-		print message
-	else:
-		message = 'Messages are different'
-		print message
-	return; 
-
-
 def createMessageOutput(cmd):
 	global testNum
 	with io.FileIO(outputMessagePath + getfileName('message.txt'), 'w') as file:
@@ -46,29 +35,39 @@ def addMessageOutput(message):
 			file.write(message)
 
 
-def runJSONToCSV(csvFields, inputFile, outputPathFile):
+def runJSONToCSV(csvFields, inputPathFile, outputPathFile):
+	outputMessageFile = outputMessagePath + getfileName('message.txt')
+	expectedMessageFile = expectedMessagePath + getfileName('ExpectedMessage.txt')
 	outputMessageFile = outputMessagePath + getfileName('message.txt')
 	outputFile = outputFilePath + getfileName(outputPathFile)
+	inputFile = inputFilePath + inputPathFile
 
-	cmd = 'json2csv -k ' + csvFields + ' -i ' + inputFilePath + inputFile + ' -o ' + outputFile
+	cmd = 'json2csv -k ' + csvFields + ' -i ' + inputFile + ' -o ' + outputFile
 	print 'running cmd ' + cmd + '\n'
+	createMessageOutput(cmd + '\n\n');
 
-	if os.path.exists(outputFile):
-		#createMessageOutput(cmd + '\n\n')
+	if os.path.exists(inputFile) == False:
+		#file path of input file is incorrect
+		print 'test message is output to ' + outputMessageFile + '\n'
 
+		message = 'Path to input file is invalid'
+		print message
+		addMessageOutput(message)
+	elif os.path.exists(outputFile) == False:
+		#file path of output file is incorrect
+		print 'test message is output to ' + outputMessageFile + '\n'
+
+		message = 'Path to output file is invalid'
+		print message
+		addMessageOutput(message)
+	else:
 		#run system command
 		os.system(cmd)
 
 		#compare files
 		compareFilesOutput_pythonCMP(outputPathFile)
-	else:
-		#file path of output file is incorrect
-		print 'test message is output to ' + outputMessageFile + '\n'
 
-		message = 'Path to output file is invalid, TEST FAILED'
-		print message
-		createMessageOutput(message + '\n\n')
-
+	compareExpectedMessageOutput(outputMessageFile, expectedMessageFile)
 	return; 
 
 
@@ -82,24 +81,37 @@ def compareFilesOutput_pythonCMP(outputPathFile):
 	print 'running cmd ' + cmd + '\n'
 
 	os.system(cmd)
+	addMessageOutput(cmd + '\n\n');
 
-	#addMessageOutput(cmd + '\n\n')
-	#os.system(cmd + ' >> ' + outputMessageFile)
 	#os.system(cmd + ' >> ' + outputMessageFile)
 
 	print 'test message is output to ' + outputMessageFile + '\n'
 
 	fileIsSame = filecmp.cmp(outputFile, expectedOutputFile)
 	if fileIsSame:
-		message = 'Both Files are identical, TEST PASSED'
+		message = 'Both Files are identical'
 		print message
 		addMessageOutput(message)
 	else:
-		message = 'Files are different, TEST FAILED'
+		message = 'Both Files are different'
 		print message
 		addMessageOutput(message)
+	return; 
 
-	compareExpectedMessageOutput(outputMessageFile, expectedMessageFile)
+
+def compareExpectedMessageOutput(messageFile, expectedMessageFile):
+
+	print '\ncomparing message to expected message: \n'
+	print messageFile + '\n'
+	print expectedMessageFile + '\n'
+
+	messageIsSame = filecmp.cmp(messageFile, expectedMessageFile)
+	if messageIsSame:
+		message = 'Both messages are identical, TEST PASSED!'
+		print message
+	else:
+		message = 'Both Messages are different, TEST FAILED!'
+		print message
 	return; 
 
 
@@ -120,46 +132,46 @@ def testFrame(testName, csvFields, inputFile, outputPathFile):
 testFrame('Testing Valid Input Path, Empty File', 'nonExistentField', 'test1_input.json', 'output.csv')
 
 
-#test 2
+# #test 2
 #VALID_JSON_PATH = FALSE, FILE_CONTENTS = EMPTY, ALL_VALID_JSON_OBJECTS = FALSE, EACH_ROW_HAS_ONE_JSON_OBJECT = FALSE, FIELDS_EXIST_IN_JSON_OBJ = FALSE, VALID_OUTPUT_PATH = FALSE
 testFrame('Testing Invalid Input Path, Empty File', 'nonExistentField', 'invalidPath/nonExistentFile.json', 'output.csv')
 
 
-# #test 3
+# # #test 3
 #VALID_JSON_PATH = TRUE, FILE_CONTENTS = MULTIPLE_ROWS, ALL_VALID_JSON_OBJECTS = TRUE, EACH_ROW_HAS_ONE_JSON_OBJECT = TRUE, FIELDS_EXIST_IN_JSON_OBJ = TRUE, VALID_OUTPUT_PATH = TRUE
 testFrame('Testing Valid Input Path, Multiple Rows File, Valid Json Obj, Each row has one jason Obj, Fields exist in Json Obj, Valid Output Path', 'user.name,remote_ip', 'test3_input.json', 'output.csv')
 
 
-# #test 4
-#VALID_JSON_PATH = TRUE, FILE_CONTENTS = SINGLE_ROW, ALL_VALID_JSON_OBJECTS = TRUE, EACH_ROW_HAS_ONE_JSON_OBJECT = FALSE, FIELDS_EXIST_IN_JSON_OBJ = TRUE, VALID_OUTPUT_PATH = FALSE
-testFrame('Testing Valid Input Path, Single Row File, Valid Json Obj, Each row does not have one jason Obj, Fields exist in Json Obj, Invalid Output Path', 'user.name,remote_ip', 'test4_input.json', 'invalidPath/nonExistentFile.csv')
+# # #test 4
+# #VALID_JSON_PATH = TRUE, FILE_CONTENTS = SINGLE_ROW, ALL_VALID_JSON_OBJECTS = TRUE, EACH_ROW_HAS_ONE_JSON_OBJECT = FALSE, FIELDS_EXIST_IN_JSON_OBJ = TRUE, VALID_OUTPUT_PATH = FALSE
+# testFrame('Testing Valid Input Path, Single Row File, Valid Json Obj, Each row does not have one jason Obj, Fields exist in Json Obj, Invalid Output Path', 'user.name,remote_ip', 'test4_input.json', 'invalidPath/nonExistentFile.csv')
 
 
-# #test 5
-#VALID_JSON_PATH = TRUE, FILE_CONTENTS = MULTIPLE_ROWS, ALL_VALID_JSON_OBJECTS = FALSE, EACH_ROW_HAS_ONE_JSON_OBJECT = TRUE, FIELDS_EXIST_IN_JSON_OBJ = FALSE, VALID_OUTPUT_PATH = FALSE
-testFrame('Testing Valid Input Path, Multiple Rows File, Invalid Json Obj, Each row has one jason Obj, Fields do not exist in Json Obj, Invalid Output Path', 'nonExistentField,nonExistentField2', 'test5_input.json', 'invalidPath/nonExistentFile.csv')
+# # #test 5
+# #VALID_JSON_PATH = TRUE, FILE_CONTENTS = MULTIPLE_ROWS, ALL_VALID_JSON_OBJECTS = FALSE, EACH_ROW_HAS_ONE_JSON_OBJECT = TRUE, FIELDS_EXIST_IN_JSON_OBJ = FALSE, VALID_OUTPUT_PATH = FALSE
+# testFrame('Testing Valid Input Path, Multiple Rows File, Invalid Json Obj, Each row has one jason Obj, Fields do not exist in Json Obj, Invalid Output Path', 'nonExistentField,nonExistentField2', 'test5_input.json', 'invalidPath/nonExistentFile.csv')
 
 
-# #test 6
-#VALID_JSON_PATH = TRUE, FILE_CONTENTS = SINGLE_ROW, ALL_VALID_JSON_OBJECTS = FALSE, EACH_ROW_HAS_ONE_JSON_OBJECT = TRUE, FIELDS_EXIST_IN_JSON_OBJ = TRUE, VALID_OUTPUT_PATH = TRUE
-testFrame('Testing Valid Input Path, Single Row File, Invalid Json Obj, Each row has one jason Obj, Fields exist in Json Obj, Valid Output Path', 'user.name,remote_ip', 'test6_input.json', 'output.csv')
-
-
-
-# #test 7
-#VALID_JSON_PATH = TRUE, FILE_CONTENTS = MULTIPLE_ROWS, ALL_VALID_JSON_OBJECTS = TRUE, EACH_ROW_HAS_ONE_JSON_OBJECT = FALSE, FIELDS_EXIST_IN_JSON_OBJ = TRUE, VALID_OUTPUT_PATH = TRUE
-testFrame('Testing Valid Input Path, Multiple Rows File, Invalid Json Obj, Each row does not have one jason Obj, Fields exist in Json Obj, Valid Output Path', 'user.name,remote_ip', 'test7_input.json', 'invalidPath/nonExistentFile.csv')
-
-
-# #test 8
-#VALID_JSON_PATH = TRUE, FILE_CONTENTS = SINGLE_ROW, ALL_VALID_JSON_OBJECTS = FALSE, EACH_ROW_HAS_ONE_JSON_OBJECT = FALSE, FIELDS_EXIST_IN_JSON_OBJ = FALSE, VALID_OUTPUT_PATH = TRUE
-testFrame('Testing Valid Input Path, Single Row File, Invalid Json Obj, Each row does not have one jason Obj, Fields do not exist in Json Obj, Valid Output Path', 'nonExistentField,nonExistentField2', 'test8_input.json', 'output.csv')
+# # #test 6
+# #VALID_JSON_PATH = TRUE, FILE_CONTENTS = SINGLE_ROW, ALL_VALID_JSON_OBJECTS = FALSE, EACH_ROW_HAS_ONE_JSON_OBJECT = TRUE, FIELDS_EXIST_IN_JSON_OBJ = TRUE, VALID_OUTPUT_PATH = TRUE
+# testFrame('Testing Valid Input Path, Single Row File, Invalid Json Obj, Each row has one jason Obj, Fields exist in Json Obj, Valid Output Path', 'user.name,remote_ip', 'test6_input.json', 'output.csv')
 
 
 
-# #test 9
-#VALID_JSON_PATH = TRUE, FILE_CONTENTS = EMPTY, ALL_VALID_JSON_OBJECTS = FALSE, EACH_ROW_HAS_ONE_JSON_OBJECT = FALSE, FIELDS_EXIST_IN_JSON_OBJ = FALSE, VALID_OUTPUT_PATH = FALSE
-testFrame('Testing Valid Input Path, Empty Field, Invalid Json Obj, Each row does not have one jason Obj, Fields do not exist in Json Obj, Invalid Output Path', 'nonExistentField,nonExistentField2', 'test9_input.json', 'output.csv')
+# # #test 7
+# #VALID_JSON_PATH = TRUE, FILE_CONTENTS = MULTIPLE_ROWS, ALL_VALID_JSON_OBJECTS = TRUE, EACH_ROW_HAS_ONE_JSON_OBJECT = FALSE, FIELDS_EXIST_IN_JSON_OBJ = TRUE, VALID_OUTPUT_PATH = TRUE
+# testFrame('Testing Valid Input Path, Multiple Rows File, Invalid Json Obj, Each row does not have one jason Obj, Fields exist in Json Obj, Valid Output Path', 'user.name,remote_ip', 'test7_input.json', 'invalidPath/nonExistentFile.csv')
+
+
+# # #test 8
+# #VALID_JSON_PATH = TRUE, FILE_CONTENTS = SINGLE_ROW, ALL_VALID_JSON_OBJECTS = FALSE, EACH_ROW_HAS_ONE_JSON_OBJECT = FALSE, FIELDS_EXIST_IN_JSON_OBJ = FALSE, VALID_OUTPUT_PATH = TRUE
+# testFrame('Testing Valid Input Path, Single Row File, Invalid Json Obj, Each row does not have one jason Obj, Fields do not exist in Json Obj, Valid Output Path', 'nonExistentField,nonExistentField2', 'test8_input.json', 'output.csv')
+
+
+
+# # #test 9
+# #VALID_JSON_PATH = TRUE, FILE_CONTENTS = EMPTY, ALL_VALID_JSON_OBJECTS = FALSE, EACH_ROW_HAS_ONE_JSON_OBJECT = FALSE, FIELDS_EXIST_IN_JSON_OBJ = FALSE, VALID_OUTPUT_PATH = FALSE
+# testFrame('Testing Valid Input Path, Empty Field, Invalid Json Obj, Each row does not have one jason Obj, Fields do not exist in Json Obj, Invalid Output Path', 'nonExistentField,nonExistentField2', 'test9_input.json', 'output.csv')
 
 
 
